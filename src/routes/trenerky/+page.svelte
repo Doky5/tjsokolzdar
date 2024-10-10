5d9fbaa712ab588cf0d84896890e2934ea310961
Doky5 <jdokulil5@gmail.com>
2024-10-10 17:33:28 +0200

    trenerky

---
+++ src/routes/trenerky/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { fade } from 'svelte/transition';
+	import Trenerky from '../../content/Trenerky.mdx';
+</script>
+
+<main in:fade>
+	<article class="trenerky">
+		<Trenerky />
+	</article>
+</main>
